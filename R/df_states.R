971238793db2c388e2b5017a5b66317ef0acd922
Patrick <pndrake@umich.edu>
2024-10-11 15:26:34 -0400

    Update df_states.R

--- R/df_states.R
+++ R/df_states.R
@@ -4,3 +4,4 @@ df_states <- data.frame(State = state.name, Abbr = state.abb) %>%
   dplyr::add_row(State = "United States", Abbr = "US") %>%
   dplyr::add_row(State = "District of Columbia", Abbr = "DC") 
 
+
